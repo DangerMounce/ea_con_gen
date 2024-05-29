86147c67018cef45f5935de46682e9e5748be320
DangerMounce <chrmounce@gmail.com>
2024-05-29 22:41:25 +0100

    14.3.7 Update

--- modules/utils.js
+++ modules/utils.js
@@ -32,7 +32,7 @@ import { callStatusMessage, statusMessage } from './library_sync.js';
 const ea_con_gen = "ea_con_gen"
 const helpVersion = '14.0' // when updating, prev version in here so that we know how old help is.
 
-const appVersion = '14.3.6' 
+const appVersion = '14.3.7' 
 
 // This function returns the current date
 export function getDate() {
