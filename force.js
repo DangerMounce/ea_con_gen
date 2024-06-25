3694f9f33b53e4cd4e18bc5d781fd50a909a378b
DangerMounce <chrmounce@gmail.com>
2024-06-25 14:35:57 +0100

    change

--- force.js
+++ force.js
@@ -7,13 +7,6 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import AdmZip from 'adm-zip';
-import inquirer from 'inquirer';
-import chalk from 'chalk';
-import { spinner } from './progress.js';
-import { display } from './display.js'
-import { menu } from './menus.js';
-
-
 
 // Configuration
 const repoOwner = 'DangerMounce';
@@ -27,25 +20,6 @@ const downloadUrl = `https://github.com/${repoOwner}/${repoName}/archive/refs/he
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const updateDir = path.resolve(__dirname, '..');  //`auto_update.js` is in the `modules` directory
 const versionFilePath = path.resolve(updateDir, 'version.log');
-
-async function promptUserToUpdate() {
-    try {
-        const readyToUpdate = await inquirer.prompt([
-            {
-                type: 'confirm',
-                name: 'confirmation',
-                message: chalk.bold.yellow('Update?'),
-                default: false // Set default value as needed
-            }
-        ]);
-
-        const confirmation = readyToUpdate.confirmation;
-        return confirmation;
-    } catch (error) {
-        console.error(chalk.red(`Error: ${error.message}`));
-        process.exit(1);
-    }
-}
 
 async function getLatestVersion() {
     const response = await fetch(gitHubUrl);
@@ -66,33 +40,6 @@ function writeCurrentVersion(version) {
     // console.log(chalk.bold.yellow('==>'), `Current version (${version}) written to version.log`);
 }
 
-async function checkForUpdates() {
-    const currentVersion = await getCurrentVersion();
-    const latestVersion = await getLatestVersion();
-    if (currentVersion !== latestVersion) {
-        console.clear('')
-        console.log('')
-        console.log(chalk.yellow('Hey 👋 - an update for ea_con_gen is available.'));
-        console.log('')
-        const updateAgreed = await promptUserToUpdate();
-        if (updateAgreed) {
-            spinner.updatingApp()
-            await updateRepository();
-            writeCurrentVersion(latestVersion);
-            spinner.stopAnimation()
-            console.clear()
-            await display.figletText('ea_con_gen');
-            console.log('');
-            // await displayChangeLog(); // Await this function to ensure the change log is displayed
-            console.log('');
-            console.log(chalk.bold.yellow('Update completed successfully.'));
-            console.log(chalk.bold.yellow('Please restart the script to apply the updates.'));
-            process.exit(1);
-        }
-        return;
-    } else {
-    }
-}
 
 async function updateRepository() {
     try {
@@ -135,13 +82,10 @@ async function extractZip(zipPath, dest) {
 }
 
 async function forceUpdate() {
-    const updateAgreed = await promptUserToUpdate();
+
     const chatVersionFilePath = path.join('chatVersion.log');
     const callVersionFilePath = path.join('callVersion.log');
-
-    if (updateAgreed) {
-        await updateRepository();
-    }
+    await updateRepository();
 
     const files = [chatVersionFilePath, callVersionFilePath];
 
@@ -155,28 +99,6 @@ async function forceUpdate() {
     });
 }
 
-export async function displayChangeLog() {
-    const filePath = path.join('ea_con_modules', 'change.log');
-    try {
-        const data = await fs.promises.readFile(filePath, 'utf8');
-        const lines = data.split('\n');
-        lines.forEach(line => {
-            if (line.includes('Enhancements')) {
-                console.log(chalk.bold.green(line));
-            } else if (line.includes('Bug Fixes')) {
-                console.log(chalk.bold.green(line));
-            } else if (line.includes('Info')) {
-                console.log(chalk.bold.green(line));
-            } else if (line.includes('Version')) {
-                console.log(chalk.bold.yellow(line))
-            } else {
-                console.log(line);
-            }
-        });
-    } catch (error) {
-        console.error('Change Log not found.');
-    }
-}
 
 
 forceUpdate()
